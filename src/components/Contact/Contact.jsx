c9ad1e9751932900b0360964558044b42e7585d1
Crooks2k <ciberronaldo@hotmail.com>
2022-12-27 19:57:31 -0500

    Contact modal => email form create (not funcional - BETA)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,76 @@
-import React from 'react'
-import ContactEmailPop from './ContactEmailPop';
 import email from "../../images/Email.png"
 import git from "../../images/mdi_github.png"
 import linkedin from "../../images/mdi_linkedin.png"
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 function Contact() {
-    function gitHubRedirect(e){
+    //github redirect funct
+    function gitHubRedirect(e) {
         e.preventDefault();
         window.open("https://github.com/Crooks2k", '_blank');
     }
-    function LinkedinRedirect(e){
+    //linkedin redirect funct
+    function LinkedinRedirect(e) {
         e.preventDefault();
         window.open("https://www.linkedin.com/in/daniel-felipe-veloza-0a00b8208/", '_blank');
     }
-    function EmailPopUpForm(e){
-        e.preventDefault();
-        <ContactEmailPop/>
-    }
 
-    
-  return (
-    <div className='Contact'>
-        <div className="Contact-display">
-            <img src={email} alt="Email Contact" className='contact-images'/>
-            <img src={linkedin} alt="Linkedin Contact" className='contact-images' onClick={LinkedinRedirect}/>
-            <img src={git} alt="Github Contact" className='contact-images' onClick={gitHubRedirect}/>
+    //Modal variables (state)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+        <div className='Contact'>
+            <div className="Contact-display">
+                {/* Email Modal start */}
+                <img src={email} alt="Email Contact" className='contact-images' onClick={handleShow}/>
+
+                <Modal show={show} onHide={handleClose} backdrop="static" centered class="modal-main" >
+                    <Modal.Header closeButton className="modal-header__bg">
+                        <Modal.Title id="modal-tittle-main">Enviar correo de contacto</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="modal-bg">
+                        <Form>
+                           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Nombre</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Nombre de empresa o remitente"
+                                    autofocus
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com"
+                                    
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlTextarea1"
+                            >
+                                <Form.Label>Razón de contacto</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer className="modal-footer__bg">
+                        <Button variant="primary" onClick={handleClose} className="Modal-send__but">
+                            Enviar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                {/* Email Modal end */}
+            <img src={linkedin} alt="Linkedin Contact" className='contact-images' onClick={LinkedinRedirect} />
+            <img src={git} alt="Github Contact" className='contact-images' onClick={gitHubRedirect} />
         </div>
-    </div>
+    </div >
   )
 }
 
--- src/components/Contact/ContactEmailPop.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ContactEmailPop() {
-  return (
-    <div>ContactEmailPop</div>
-  )
-}
-
-export default ContactEmailPop
